7836c1e63329e5bec9fbb59513171e13acba14c3
Elvin Martinez <43117188+Elvin308@users.noreply.github.com>
2025-09-08 08:32:00 -0500

    Added keydown events for the buttons to play their coresponding sounds

--- index.js
+++ index.js
@@ -17,3 +17,32 @@ document.querySelectorAll(".drum-button").forEach((button, index) =>{
         }
     });
 });
+
+//Add event listener for keyboard down
+document.addEventListener("keydown", (event) => {
+    switch(event.key.toUpperCase()){
+        case "K":
+            new Audio("sounds/crash.mp3").play();
+            break;
+        case "L":
+            new Audio("sounds/kick-bass.mp3").play();
+            break;
+        case "J":
+            new Audio("sounds/snare.mp3").play();
+            break;
+        case "W":
+            new Audio("sounds/tom-1.mp3").play();
+            break;
+        case "A":
+            new Audio("sounds/tom-2.mp3").play();
+            break;
+        case "S":
+            new Audio("sounds/tom-3.mp3").play();
+            break;
+        case "D":
+            new Audio("sounds/tom-4.mp3").play();
+            break;
+        default:
+            console.log(event.key + "Not implemented for sound");
+    }
+});
