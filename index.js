8ed59ff89fe2accaa581b383d66bc8a3c8a8691b
Elvin Martinez <43117188+Elvin308@users.noreply.github.com>
2025-09-08 08:17:27 -0500

    Added sound for remaining buttons using a switch statement

--- index.js
+++ index.js
@@ -2,6 +2,18 @@
 //Play sound for each button when clicked
 document.querySelectorAll(".drum-button").forEach((button, index) =>{
     button.addEventListener("click", () => {
-        new Audio("sounds/tom-" +(index + 1)+ ".mp3").play();
+        switch(button.innerHTML){
+            case "K":
+                new Audio("sounds/crash.mp3").play();
+                break;
+            case "L":
+                new Audio("sounds/kick-bass.mp3").play();
+                break;
+            case "J":
+                new Audio("sounds/snare.mp3").play();
+                break;
+            default:
+                new Audio("sounds/tom-" +(index + 1)+ ".mp3").play();
+        }
     });
 });
